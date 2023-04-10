4cfb25630be8b57ab3a2d46d4b184807ebee3384
Jayson Baker <bakej07@gmail.com>
2023-04-09 22:44:46 -0500

    Fixed localstorage and button bugs. WIP button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,93 +7,108 @@ let searchNumber = localStorage.length;
 function getWeather(event) {
   event.preventDefault();
   const cityName = $("#search").val();
-  const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=acbf659b6dad995f4221a78b638e6923`;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=acbf659b6dad995f4221a78b638e6923`;
   fetch(weatherUrl).then(function (response) {
     response.json().then(function (data) {
-      for (let i = 0; i < 6; i++) {
-        let temp = `Temp: ${(
-          (data.list[i].main.temp - 273.15) * 1.8 +
-          32
-        ).toFixed(2)}°F`;
-        //
-        let wind = `Wind: ${data.list[i].wind.speed}mph`;
-        //
-        let humidity = `Humidity: ${data.list[i].main.humidity}%`;
-        //
-        let date = data.list[i].dt_txt.split(" ")[0].replaceAll("-", "/");
-        //
-        let weatherIcon = data.list[i].weather[0].description;
-        // console.log(temp, wind, humidity, date, weatherIcon);
-        //
-        if (i != 0) {
-          let card = $("<div>");
-          card.addClass("card w-auto mx-3");
-          fiveDayForcast.append(card);
+      console.log(data);
+      for (let i = 0; i < data.list.length; i++) {
+        const forecastDate = new Date(data.list[i].dt_txt);
+        if (
+          data.list[i].dt_txt.split(" ")[1] ===
+          data.list[0].dt_txt.split(" ")[1]
+        ) {
+          let temp = `Temp: ${data.list[i].main.temp.toFixed(2)}°F`;
           //
-          let cardBody = $("<div>");
-          cardBody.addClass("card-body");
-          card.append(cardBody);
+          let wind = `Wind: ${data.list[i].wind.speed}mph`;
           //
-          let cardTitle = $("<h3>");
-          cardTitle.addClass("card-title");
-          cardTitle.text(date);
-          cardBody.append(cardTitle);
+          let humidity = `Humidity: ${data.list[i].main.humidity}%`;
           //
-          let cardIcon = $("<div>");
-          cardIcon.addClass("card-subtitle");
-          cardIcon.text(weatherIcon);
-          cardBody.append(cardIcon);
+          let date = data.list[i].dt_txt.split(" ")[0].replaceAll("-", "/");
           //
-          let cardTemp = $("<p>");
-          cardTemp.addClass("card-text");
-          cardTemp.text(temp);
-          cardBody.append(cardTemp);
+          let weatherIcon = data.list[i].weather[0].icon;
+          // console.log(temp, wind, humidity, date, weatherIcon);
           //
-          let cardWind = $("<p>");
-          cardWind.addClass("card-text");
-          cardWind.text(wind);
-          cardBody.append(cardWind);
-          //
-          let cardHumidity = $("<p>");
-          cardHumidity.addClass("card-text");
-          cardHumidity.text(humidity);
-          cardBody.append(cardHumidity);
-          //
-        } else {
-          let name = $("<h1>");
-          name.text(`${cityName} (${date}) ${weatherIcon}`);
-          currentCity.append(name);
-          //
-          let mainTemp = $("<h3>");
-          mainTemp.text(temp);
-          currentCity.append(mainTemp);
-          //
-          let mainWind = $("<h3>");
-          mainWind.text(wind);
-          currentCity.append(mainWind);
-          //
-          let mainHumidity = $("<h3>");
-          mainHumidity.text(humidity);
-          currentCity.append(mainHumidity);
-          //
+
+          if (i != 0) {
+            let card = $("<div>");
+            card.addClass("card w-auto mx-3");
+            fiveDayForcast.append(card);
+            //
+            let cardBody = $("<div>");
+            cardBody.addClass("card-body");
+            card.append(cardBody);
+            //
+            let cardTitle = $("<h3>");
+            cardTitle.addClass("card-title");
+            cardTitle.text(date);
+            cardBody.append(cardTitle);
+            //
+            let cardIcon = $("<img>");
+            cardIcon.addClass("img-fluid");
+            cardIcon.attr(
+              "src",
+              `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+            );
+
+            cardBody.append(cardIcon);
+            //
+            let cardTemp = $("<p>");
+            cardTemp.addClass("card-text");
+            cardTemp.text(temp);
+            cardBody.append(cardTemp);
+            //
+            let cardWind = $("<p>");
+            cardWind.addClass("card-text");
+            cardWind.text(wind);
+            cardBody.append(cardWind);
+            //
+            let cardHumidity = $("<p>");
+            cardHumidity.addClass("card-text");
+            cardHumidity.text(humidity);
+            cardBody.append(cardHumidity);
+            //
+          } else {
+            let name = $("<h1>");
+            name.text(`${cityName} (${date})`);
+            currentCity.append(name);
+            //
+            let mainIcon = $("<img>");
+            mainIcon.attr(
+              "src",
+              `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+            );
+            name.append(mainIcon);
+            //
+            let mainTemp = $("<h3>");
+            mainTemp.text(temp);
+            currentCity.append(mainTemp);
+            //
+            let mainWind = $("<h3>");
+            mainWind.text(wind);
+            currentCity.append(mainWind);
+            //
+            let mainHumidity = $("<h3>");
+            mainHumidity.text(humidity);
+            currentCity.append(mainHumidity);
+            //
+          }
         }
       }
       searchNumber++;
-      localStorage.setItem(`History ${searchNumber}`, cityName);
+      localStorage.setItem(cityName.toUpperCase(), cityName);
     });
   });
 }
 
+function genButton() {}
+
 function getHistory() {
-  for (let i = localStorage.length; i > 0; i--) {
-    let history = localStorage.getItem(`History ${i}`);
-    console.log(history);
+  for (const [key, value] of Object.entries(localStorage)) {
     let historyButton = $("<button>");
-    historyButton.addClass("btn btn-primary w-100 m-1");
-    historyButton.text(history);
+    historyButton.addClass("btn btn-primary w-100 m-1 historyBtn");
+    historyButton.text(value);
     userOptions.append(historyButton);
   }
 }
-
 window.addEventListener("load", getHistory);
 searchBtn.on("click", getWeather);
